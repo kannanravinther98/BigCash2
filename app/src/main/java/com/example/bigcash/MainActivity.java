2f15f6327570066f75385f6077b473539e4370b4
Kannanak96170 <kannanravinther@gmail.com>
2021-01-01 23:55:48 +0800

    3 pages of User Onboarding done.

---
+++ app/src/main/java/com/example/bigcash/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.bigcash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToLoginPage(View view) {
+        Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(intent);
+    }
+    public void goToRegisterPage(View view) {
+        Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+        startActivity(intent);
+
+    }
+}
