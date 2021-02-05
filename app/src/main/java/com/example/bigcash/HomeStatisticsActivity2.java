c7943312ba1a06e4d9dadec901363fdff59e774f
Kannanak96170 <kannanravinther@gmail.com>
2021-02-05 10:52:18 +0800

    Icons

--- app/src/main/java/com/example/bigcash/HomeStatisticsActivity2.java
+++ app/src/main/java/com/example/bigcash/HomeStatisticsActivity2.java
@@ -1,34 +1,14 @@
 package com.example.bigcash;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-
-import java.util.ArrayList;
 
 public class HomeStatisticsActivity2 extends AppCompatActivity {
     private WebView webView;
-    private Button btn;
-    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
